b5107ec9692db9c1bfaf2222a10da8f9d6208e31
RevanDTH <davidkrammer21@gmail.com>
2025-11-02 20:49:43 +0100

    Update app title and HTML title for consistency

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('expenses-manager-tmp');
+  protected readonly title = signal('expenses-manager');
 }
